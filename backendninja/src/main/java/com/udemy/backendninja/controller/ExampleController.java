69a8cc0e2f1a536191798991488c3bcc7deddd68
Jose Luis Gonzalez Villalba <jose.l.gonzalez.villalba.-nd@disney.com>
2018-03-22 14:54:30 -0600

    Se agrega lógica de controler para iterar una lista en la vista con TH - t22

--- backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -1,5 +1,8 @@
 package com.udemy.backendninja.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,23 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.backendninja.model.Person;
+import com.udemy.backendninja.service.IPersonaService;
+
 @Controller
 @RequestMapping("/example")
 public class ExampleController {
 	
 	public static final String EXAMPLE_VIEW="example";
 	
+	@Autowired
+	private IPersonaService personaService;
+	
 	//@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	@GetMapping("/exampleString")
 	public String exampleString(Model model){
-		model.addAttribute("name", "Luis");
+		model.addAttribute("listPeople", personaService.listPersons());
 		return EXAMPLE_VIEW;
 	}
 	
 	@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
 	public ModelAndView example(){
 		ModelAndView mav = new ModelAndView(EXAMPLE_VIEW);
-		mav.addObject("name", "Mikel");
+		mav.addObject("listPeople", personaService.listPersons());
 		return mav;
 	}
 }
