f4ac707a3a8852606e030d111230051465004a63
GONZJ711 <Jose.L.Gonzalez.Villalba.-ND@disney.com>
2018-03-24 11:01:28 -0600

    Se agregan cambios en anotacion RM

--- backendninja/src/main/java/com/udemy/backendninja/controller/HelloWorldController.java
+++ backendninja/src/main/java/com/udemy/backendninja/controller/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.udemy.backendninja.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/say")
 public class HelloWorldController {
 
-	@RequestMapping("/hello_world")
+	@RequestMapping(path="/hello_world", method=RequestMethod.GET)
 	public String helloWorld() {
 		return "Hola Mundo";
 	}
