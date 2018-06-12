bd96285b0e7b9bd623e17266acd8d631519815ea
Jose Luis Gonzalez <isc.luis.gv@gmail.com>
2018-06-11 20:38:16 -0500

    Se agrega controller clase 43

--- backendninja/src/main/java/com/udemy/backendninja/service/CourseServiceImpl.java
+++ backendninja/src/main/java/com/udemy/backendninja/service/CourseServiceImpl.java
@@ -2,15 +2,20 @@ package com.udemy.backendninja.service;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import com.udemy.backendninja.controller.CourseController;
 import com.udemy.backendninja.entity.Course;
 import com.udemy.backendninja.repository.CourseJpaRepository;
 
 @Service("courseServiceImpl")
 public class CourseServiceImpl implements CourseService {
+	
+	private static final Log LOG = LogFactory.getLog(CourseServiceImpl.class);
 
 	@Autowired
 	@Qualifier("courseJpaRepository")
@@ -23,11 +28,13 @@ public class CourseServiceImpl implements CourseService {
 
 	@Override
 	public List<Course> listAllCourses() {
+		LOG.info("Calling: listAllCourses()");
 		return courseJpaRepository.findAll();
 	}
 
 	@Override
 	public void addCourse(Course course) {
+		LOG.info("Calling: addCourse()");
 		courseJpaRepository.save(course);
 	}
 
