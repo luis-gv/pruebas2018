e17b473471f4fd80c9315c5d88ebbb3efbe4edab
Jose Luis Gonzalez <isc.luis.gv@gmail.com>
2018-09-18 22:44:25 -0500

    Inicio curso java 8

--- cursoJava/src/main/java/java8curso/LambdaApp.java
+++ cursoJava/src/main/java/java8curso/LambdaApp.java
@@ -1,6 +1,8 @@
 package java8curso;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LambdaApp {
@@ -10,11 +12,22 @@ public class LambdaApp {
 		lista.add("MitoCode");
 		lista.add("Code");
 		lista.add("Mito");
+		
+		Collections.sort(lista, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		});
+		
+		for(String elemento:lista){
+			System.out.println(elemento);
+		}
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LambdaApp app = new LambdaApp();
+		app.ordenar();
 	}
 
 }
