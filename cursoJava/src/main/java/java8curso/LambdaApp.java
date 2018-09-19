9627e500d22255c3e9d6d6fda19de032f2cddb1c
Jose Luis Gonzalez <isc.luis.gv@gmail.com>
2018-09-18 22:28:51 -0500

    Agrega nuevo proyecto java 8 y configuracion de gitignor

---
+++ cursoJava/src/main/java/java8curso/LambdaApp.java
@@ -0,0 +1,20 @@
+package java8curso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaApp {
+
+	public void ordenar(){
+		List<String> lista = new ArrayList();
+		lista.add("MitoCode");
+		lista.add("Code");
+		lista.add("Mito");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
