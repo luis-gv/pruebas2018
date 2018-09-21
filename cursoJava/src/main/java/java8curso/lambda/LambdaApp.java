ccb3eb8938920fac451d1af65ae19f74b096b678
luis-gv <isc.luis.gv@gmail.com>
2018-09-21 16:57:54 -0500

    Fin Practica 1 - J8

---
+++ cursoJava/src/main/java/java8curso/lambda/LambdaApp.java
@@ -0,0 +1,48 @@
+package java8curso.lambda;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaApp {
+
+	public void ordenar(){
+		List<String> lista = new ArrayList();
+		lista.add("MitoCode");
+		lista.add("Code");
+		lista.add("Mito");
+		
+		Collections.sort(lista, (String p1, String p2) -> p1.compareTo(p2));
+		
+//		Collections.sort(lista, new Comparator<String>() {
+//			@Override
+//			public int compare(String o1, String o2) {
+//				return o1.compareTo(o2);
+//			}
+//		});
+		
+		for(String elemento:lista){
+			System.out.println(elemento);
+		}
+	}
+	
+	public void calcular(){
+		/*Operacion operacion = new Operacion() {
+			
+			@Override
+			public double calcular(double n1, double n2) {
+				return (n1+n2)/2;
+			}
+		};*/
+		Operacion operacion = (double x, double y) -> (x+y)/2;
+		System.out.println(operacion.calcular(5, 2));
+	}
+	
+	public static void main(String[] args) {
+		LambdaApp app = new LambdaApp();
+		//app.ordenar();
+		app.calcular();
+	}
+
+}
---
+++ cursoJava/src/main/java/java8curso/lambda/Operacion.java
@@ -0,0 +1,5 @@
+package java8curso.lambda;
+
+public interface Operacion {
+	double calcular(double n1, double n2);
+}
